90b2d8fa8cbbaf0af9b56677f1f191998494da7c
midnight-ranger <elbountou99@gmail.com>
2024-07-26 13:57:07 +0000

    commit

--- myApp.js
+++ myApp.js
@@ -1,10 +1,32 @@
 let express = require('express');
+const res = require('express/lib/response');
 let app = express();
+
 console.log('Hello World');
 
+app.use('/public', express.static(__dirname + '/public'));
 
 
 
+app.use(function(req, res, next){
+    console.log(`${req.method} ${req.path} - ${req.ip}`)
+});
+
+
+app.get('/json', function(req, res){
+    const data = {
+        "message": "Hello json"
+    };
+    res.json(data);
+});
+
+
+
+app.get('/', function(req, res){
+    const filePath = __dirname + '/view/index.html';
+    console.log(filePath);
+    res.sendFile(filePath);
+});
 
 
 
